d2252cfd5e48c24a627910d03351c67570c0e6aa
Mingda Jin <jinmingda77@gmail.com>
2016-10-31 10:21:18 -0700

    Add class 'PCR' to run in silico amplification

--- testprimer/pcr.py
+++ testprimer/pcr.py
@@ -48,39 +48,42 @@ class PrimerPool:
 
 class PCRMatch:
 
-    def __init__(self):
+    def __init__(self, template, fw_match, rv_match, is_amplified):
+        self.template = template
+        self.fw_match = fw_match
+        self.rv_match = rv_match
         self.is_amplified = is_amplified
-        self.classification = classification
+
 
 class PCR:
 
     def __init__(self, template, fw_primer_pool, rv_primer_pool, find_match):
-        if isinstance(template, Template):
-            return TypeError
-        if isinstance(fw_primer_pool, PrimerPool):
-            return TypeError
-        if isinstance(rv_primer_pool, PrimerPool):
-            return TypeError
+        if not isinstance(template, Template):
+            raise TypeError
+        if not isinstance(fw_primer_pool, PrimerPool):
+            raise TypeError
+        if not isinstance(rv_primer_pool, PrimerPool):
+            raise TypeError
         self.template = template
         self.fw_primer_pool = fw_primer_pool
         self.rv_primer_pool = rv_primer_pool
         self._find_match = find_match
 
     def cut_forward_primer_from_template(self):
-        start = self.forward_primer_pool.start - 1
-        end = self.forward_primer_pool.end
+        start = self.fw_primer_pool.start - 1
+        end = self.fw_primer_pool.end
         return self.template.seq[start:end]
 
     def cut_reverse_primer_from_template(self):
-        start = self.reverse_primer_pool.start - 1
-        end = self.reverse_primer_pool.end
+        start = self.rv_primer_pool.start - 1
+        end = self.rv_primer_pool.end
         return self.template.seq[start:end]
 
     def run(self):
         GAP = '-'  # gap placeholder
         UNKNOWN = '.'  # leading and trailing placeholder 
 
-        # cut primer from template and back transcribe
+        # cut primer from template and back transcribe to DNA
         fw_tmplt = self.cut_forward_primer_from_template().back_transcribe()
         rv_tmplt = self.cut_reverse_primer_from_template().back_transcribe()
 
@@ -89,21 +92,20 @@ class PCR:
         rv_tmplt = rv_tmplt.ungap(UNKNOWN).ungap(GAP)
 
         # test match
-        for fw in self.fw_primer_pool:
+        for fw in self.fw_primer_pool.primers:
             if self._find_match(str(fw), str(fw_tmplt)):
                 fw_match = True
                 break
             fw_match = False
 
-        for rv in self.rv_primer_pool:
-            if self._find_match(str(rv), str(rv_tmplt)):
+        for rv in self.rv_primer_pool.primers:
+            if self._find_match(str(rv.reverse_complement()), str(rv_tmplt)):
                 rv_match = True
                 break
             rv_match = False
 
         is_amplified = fw_match and rv_match
-
-        #return PCRMatch()
+        return PCRMatch(self.template, fw_match, rv_match, is_amplified)
 
 
 def simple_match(seq1, seq2):
